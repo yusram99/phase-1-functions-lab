496baf8b47fdcec9723e2d2f0f1d1b22dc44af9b
Your Name <youremail@example.com>
2023-06-14 18:17:17 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) *264;
+    }
+    
+    function distanceTravelledInFeet(starBlock, endBlock) {
+        return Math.abs(starBlock - endBlock) *264;
+        }
+       
+        
